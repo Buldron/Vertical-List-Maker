b17c260e8cb765e113db1aa66bc7067a1414356a
Buldron <36693901+Buldron@users.noreply.github.com>
2018-06-06 06:51:22 +1000

    Duplicate Name Bug Fix.
    Fixed bug where duplicated names would not appear in the final sorted list.

--- Program/Vertical List Maker/Program.cs
+++ Program/Vertical List Maker/Program.cs
@@ -73,7 +73,7 @@ namespace Vertical_List_Maker
 		{
 			List<string> list = new List<string>();
 			
-			for(int i = 0; i < input.Count - 1; i++)
+			for(int i = 0; i < input.Count; i++)
 			{
 				bool duplicate = false;
 				for (int j = i + 1; j < input.Count; j++)
